7cb84342b197133df119aca119484ee03ab4bca5
Taylor Smith <jtsmith2@gmail.com>
2017-05-15 20:54:53 -0400

    Update batch file with examples and readme file

--- generate_charts.bat
+++ generate_charts.bat
@@ -1,9 +1,5 @@
-::chart.py -r3 -m5 -l1 -S.5 --ngc_max 6 --out r3index.png
-::chart.py -r9 -m5 -l1 -S.5 --ngc_max 6 --out r9index.png
-::chart.py -r15 -m5 -l1 -S.5 --ngc_max 6 --out r15index.png
-::chart.py -r21 -m5 -l1 -S.5 --ngc_max 6 --out r21index.png
+:: Shows the star map for Oxford, MS at 12:30PM
+python chart.py --lat 34.3665 --lon -89.5192 --utc "2012-02-01 12:30:00" --out "Oxford.png"
 
-::chart.py -r3  -l6  --ngc_max 9 --out r3_high.png
-chart.py -r9  -l6  --ngc_max 9  --out r9_high.png
-::chart.py -r15 -l6  --ngc_max 9  --out r15_high.png
-::chart.py -r21 -l6  --ngc_max 9  --out r21_high.png
+:: Shows the star map for Oxford, MS at 12:30PM with no NGC objects, star labels, constellation borders, or constellation names
+python chart.py --lat 34.3665 --lon -89.5192 --utc "2012-02-01 12:30:00" --draw_ngc -L -B --names --out "Oxford_clean.png"
